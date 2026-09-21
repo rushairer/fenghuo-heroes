73f717c7fa06c4da204d75a6c385cc25964e2173
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-21 17:27:07 +0800

    scenario: import city-state fields from neutral contract

--- src/game/scenario-evidence-template.js
+++ src/game/scenario-evidence-template.js
@@ -1,4 +1,4 @@
-import { CITY_ECONOMY_FIELDS } from './scenario-start-state.js'
+import { CITY_ECONOMY_FIELDS } from './scenario-fields.js'
 import { ZH_ROM_CANONICAL_CITY_SET } from './original-data.js'
 import { targetScenario } from './scenario-target.js'
 
