a6575f0bab1f97e2b66f161a7278cec8238b00db
Aben <5195693+rushairer@users.noreply.github.com>
2026-09-21 16:22:53 +0800

    title: integrate garment contours before silhouette depth

--- src/game/title-art.js
+++ src/game/title-art.js
@@ -1,4 +1,5 @@
 import { drawTitleFaceStructure } from './title-face-art.js'
+import { drawTitleGarmentContour } from './title-garment-contour.js'
 import { drawTitleHdDetail } from './title-hd-detail.js'
 import { drawTitleHeadgearDetail } from './title-headgear-art.js'
 import { drawTitleLocalLight } from './title-local-light.js'
@@ -214,6 +215,7 @@ export function drawTitleComposition(r) {
   drawTitleFaceStructure(r)
   drawTitleHeadgearDetail(r)
   drawTitleLocalLight(r)
+  drawTitleGarmentContour(r)
   drawTitleSilhouetteDepth(r)
   drawTitleHdDetail(r)
   drawBrocadeStrip(r)
